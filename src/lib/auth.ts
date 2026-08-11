1d2e8c567ef3316e655456e4a5bc7cfadbd79b94
AGiggleSniffer <97860788+AGiggleSniffer@users.noreply.github.com>
2026-08-10 19:45:00 -0700

    dynamic signups

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,13 +2,14 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/server/db";
 
-let alreadySeededAdmin: boolean;
-
-try {
-  alreadySeededAdmin = (await db.query.user.findFirst()) ? true : false;
-} catch {
-  alreadySeededAdmin = false;
+const alreadySeededAdmin = async () => {
+  try {
+    return (await db.query.user.findFirst()) ? true : false;
+  } catch {
+    return false;
+  }
 }
+
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
